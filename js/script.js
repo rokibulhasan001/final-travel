3234005c9d50acf910fdaeff0cf40608b2bd7887
rokibulhasan001 <164499990+rokibulhasan001@users.noreply.github.com>
2024-09-08 17:33:04 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+  AOS.init({
+     duration: 1000,
+
+  });
+
+
+
+  // javaScript code here
+
+// jQuery code here
+$(function () {
+  // preloader start
+  $(window).load(function () {
+    $("#loading").fadeOut(3000);
+  });
+  // preloader end
+});
